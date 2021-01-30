c385727d58da66be5f69def2eeb9b7b626758e57
george redinger <george@georgeredinger.com>
2021-01-30 14:11:24 -0800

    basic chimney recorder

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
 #ifndef UNIT_TEST
-#include <Arduino.h>
 #include "OTA.h"
 #include "battery.h"
 #include "mqtt.h"
 #include "propane.h"
 #include "sleep.h"
+#include <Arduino.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <TelnetStream.h>
+#include <Wire.h>
 #include <esp_system.h>
 #include <esp_task_wdt.h>
-#include <Wire.h>
 #include <max6675.h>
 
-int thermoDO = 19;
-int thermoCS = 23;
-int thermoCLK = 5;
+const int led = 13;
+const int ref2048 = 34;
+const int thermoDO = 19;
+const int thermoCS = 23;
+const int thermoCLK = 5;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-#define WDT_TIMEOUT 60    //Seconds
+#define WDT_TIMEOUT 10   //Seconds
 #define SLEEP_INTERVAL 5 //minutes
-
 
 int print_wakeup_reason() {
     esp_sleep_wakeup_cause_t wakeup_reason;
@@ -52,21 +53,54 @@ int print_wakeup_reason() {
 void setup() {
     float ftemperature;
     char stemperature[20];
+    char sbatteryVoltage[20];
+    int ref;
+    float fref;
+    float voltsPerCount;
+    float batteryVoltage;
+
     Serial.begin(115200);
     Serial.println("MAX6675 test");
-     esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+    esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
     esp_task_wdt_add(NULL);               //add current thread to WDT watch
+    pinMode(ref2048,INPUT);
+    pinMode(led, OUTPUT);
+    digitalWrite(led, HIGH);
+    ref = analogRead(ref2048);
+    fref = (float) ref;
+    voltsPerCount = 2.048/fref;
+    batteryVoltage = (voltsPerCount*4096.0)*(3.329/3.58);//with emperical cal
+    ftemperature = thermocouple.readFahrenheit();
+
+
     setup_mqtt();
 
-    ftemperature=thermocouple.readFahrenheit();
     sprintf(stemperature, "%2.2f", ftemperature);
+    sprintf(sbatteryVoltage, "%2.2f", batteryVoltage);
 
-    publish("chimney0/data", stemperature,5000);
-    //publish("chimney0/batt", sbatt,5000);
+    publish("chimney0/data", stemperature, 0);
+    publish("chimney0/battery", sbatteryVoltage, 5000);
     gotosleep(60 * SLEEP_INTERVAL); //minutes
-
 }
 
-void loop() {}
-  
+void loop() {
+    float ftemperature = thermocouple.readFahrenheit();
+    int ref;
+    float fref;
+    float voltsPerCount;
+    float batteryVoltage;
+
+    pinMode(led, OUTPUT);
+    digitalWrite(led, HIGH);
+    ref = analogRead(ref2048);
+    fref = (float) ref;
+    voltsPerCount = 2.048/fref;
+    batteryVoltage = voltsPerCount*4096.0;
+    Serial.printf("%f,%f,%f,%f\r\n",ftemperature,fref,voltsPerCount,batteryVoltage);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+    esp_task_wdt_reset();
+}
+
 #endif
